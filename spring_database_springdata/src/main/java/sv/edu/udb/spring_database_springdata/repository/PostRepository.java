8da8a1a30e7a0a4243e89e46c168e20f4120a3e1
Turtleurban1 <cchvez@rinnovocorp.com>
2025-08-23 14:31:06 -0600

    Semana 6

---
+++ spring_database_springdata/src/main/java/sv/edu/udb/spring_database_springdata/repository/PostRepository.java
@@ -0,0 +1,5 @@
+package sv.edu.udb.spring_database_springdata.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import sv.edu.udb.spring_database_springdata.repository.domain.Post;
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
